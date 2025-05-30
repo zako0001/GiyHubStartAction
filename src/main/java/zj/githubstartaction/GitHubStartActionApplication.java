117b143ba7912deae0ee817258132db9d5843d77
zako0001 <zako0001@stud.kea.dk>
2025-05-30 21:56:03 +0200

    Initial commit

---
+++ src/main/java/zj/githubstartaction/GitHubStartActionApplication.java
@@ -0,0 +1,13 @@
+package zj.githubstartaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubStartActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubStartActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zj/githubstartaction/GitHubStartActionApplicationTests.java
@@ -0,0 +1,13 @@
+package zj.githubstartaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubStartActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
